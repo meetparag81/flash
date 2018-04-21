bfb7b8afd10ae37ffe5dd9aebb4f36471bc51d35
meetparag81 <paragborawake81@gmail.com>
2018-04-21 09:53:15 +0530

    iterable

---
+++ My Project/src/Interface/Test1.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import java.util.Iterator;
+
+public class Test1 implements Iterable
+{
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
