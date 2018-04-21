c808914f828a49a81f00151bb14bbb7aa9938820
meetparag81 <paragborawake81@gmail.com>
2018-04-21 20:52:20 +0530

    capgeminiprogramme

---
+++ My Project/src/SimpleProgrammes/PatternAskedInCapgemini.java
@@ -0,0 +1,24 @@
+package SimpleProgrammes;
+
+public class PatternAskedInCapgemini
+{
+	
+	//* 
+	//* *
+	//* * *
+	
+
+		public void main(String[]args)
+		{
+			for(int row =1;row<=3;row++)
+			{
+				for(int col =1;col<=row;col++)
+				{
+					System.out.println("*");
+				}
+				
+			}
+			System.out.println( );
+		
+		}
+}
