dbda51b27e4af993d1f5e9a35b485d0a40bc470f
paragb <paragb@IVF111.PALASH.COM>
2018-06-04 09:51:57 +0530

    pattern added

---
+++ My Project/src/SeleniumBasic/TestngAnnotation.java
@@ -0,0 +1,37 @@
+package SeleniumBasic;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestngAnnotation 
+
+{
+	public class BeforetestandBeforemethod {
+
+        @BeforeTest
+         public void beforeTest() {
+           System.out.println("@BeforeTest");
+         } 
+        @BeforeMethod
+        public void beforemethod(){
+               System.out.print("@BeforeMethod");
+        }
+        
+        public void helper() {
+                  System.out.println("Flow");
+                }
+
+                @Test
+                public void toBeforeMethod() {
+                  helper();
+                  System.out.println("BeforeMethod");
+                }
+
+                @Test
+                public void toBeforeTest() {
+                  helper();
+                  System.out.println("BeforeTest");
+                }
+
+}
